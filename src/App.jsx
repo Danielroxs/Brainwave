ed00304d4762ba8dfec03b177e6b2850cc830190
Danielroxs <danroxsll@gmail.com>
2024-07-14 20:25:35 -0600

    Benefits section finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -8,6 +10,8 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
+        <Collaboration />
       </div>
 
       <ButtonGradient />
