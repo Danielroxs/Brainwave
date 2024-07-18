929c0b54bfbf14944381fa402f3d66a8999447ee
Danielroxs <danroxsll@gmail.com>
2024-07-18 17:08:32 -0600

    Roadmap & Footer finished, project finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -16,6 +18,8 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
 
       <ButtonGradient />
