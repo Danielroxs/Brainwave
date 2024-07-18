397727e5fb1b9c4b48e48298cfb95ac63fa867a2
Danielroxs <danroxsll@gmail.com>
2024-07-18 17:31:43 -0600

    LD

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <Section crosses className="!px-0 !py-10">
       <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
         <p className="caption text-n-4 lg:block">
-          © {new Date().getFullYear()}. All rights reserved.
+          © {new Date().getFullYear()}. All rights reserved Luis Daniel R.
         </p>
 
         <ul className="flex gap-5 flex-wrap">
